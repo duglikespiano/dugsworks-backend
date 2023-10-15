545ef6b65464d225886cafc33bc335ee86b0a290
SUHO PARK <duglikespiano@gmail.com>
2023-10-16 02:36:43 +0900

    FEATURE : CORS SETTING ADDED

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes/routes';
 
 export const app = express();
 
+const corsOptions = {
+	origin: 'http://localhost:3000',
+	optionsSuccessStatus: 200,
+};
+
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(router);
