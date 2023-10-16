86cb1a4a8fdf813ae89863c1c9d56d56cdb8c215
SUHO PARK <duglikespiano@gmail.com>
2023-10-17 03:48:32 +0900

    FEATURE : GUESTBOOK ROUTER ADDED

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import pingRouter from './pingRouter';
 import mailRouter from './mailRouter';
+import guestbookRouter from './guestbookRouter';
 
 const router = Router();
 
 router.use('/ping', pingRouter);
 router.use('/mail', mailRouter);
+router.use('/guestbook', guestbookRouter);
 
 export default router;
