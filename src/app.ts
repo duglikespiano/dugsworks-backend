69ba444a1e54bfebc049dd3dd7910efa4215137d
SUHO PARK <duglikespiano@gmail.com>
2023-10-16 21:01:16 +0900

    REFACTOR : FRONTEND DOTENV REFACTORED

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routes/routes';
+import { frontendServerEndpoint, frontendServerPort } from './env';
 
 export const app = express();
 
 const corsOptions = {
-	origin: 'http://localhost:3000',
+	origin: `${frontendServerEndpoint}/${frontendServerPort}`,
 	optionsSuccessStatus: 200,
 };
 
--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const backendServerPort = process.env.BACKEND_SERVER_PORT;
+export const frontendServerEndpoint = process.env.FRONTEND_SERVER_ENDPOINT;
+export const frontendServerPort = process.env.FRONTEND_SERVER_PORT;
 export const mailService = process.env.MAIL_SERVICE;
 export const mailServiceName = process.env.MAIL_SERVICE_NAME;
 export const mailServiceAddress = process.env.MAIL_SERVICE_ADDRESS;
